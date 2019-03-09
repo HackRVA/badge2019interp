48dd050bb8ff13e15d78ac8fd2daacb4a5c83547
Paul Bruggeman <paul@killercats.com>
2019-03-09 17:04:06 -0500

    missing binding code

---
+++ include/bindings.h
@@ -0,0 +1,62 @@
+enum {
+  MAINBUTTON,
+  UPBUTTON,
+  DOWNBUTTON,
+  LEFTBUTTON,
+  RIGHTBUTTON,
+  HOUR,
+  MIN,
+  SEC,
+  BADGEID,
+  NAME,
+  FLASHEDID,
+  FLASHADDR,
+  DRAWLCD8,
+  DRBOB_BIND,
+  LASTBINDING,
+};
+
+enum {
+    B_VARIABLE,
+    B_FUNCTION,
+};
+
+enum {
+    B_VOID,
+    B_CHAR,
+    B_CHARSTAR,
+    B_INT,
+    B_INTSTAR,
+};
+
+/*
+   8 bytes
+*/
+struct param_t {
+    char *name;
+    unsigned var_fun:1;
+    unsigned ret:3;
+    unsigned arg0:4;
+    unsigned arg1:4;
+    unsigned arg2:4;
+    unsigned arg3:4;
+    unsigned arg4:4;
+    unsigned arg5:4;
+    unsigned arg6:4;
+};
+
+/*
+   4 bytes
+*/
+union var_fun {
+    int (*fun)();
+    int *iv;
+    char *cv;
+};
+
+struct binding_t {
+    union var_fun vf;
+};
+
+extern struct binding_t bindings[];
+extern struct param_t parms[];
---
+++ src/bindings.c
@@ -0,0 +1,177 @@
+#include "bindings.h"
+
+/*
+
+cc -m32 -I./include -o bindings src/bindings.c
+./bindings
+
+
+#define MAIN
+*/
+#ifdef MAIN
+#include "flash.h"
+#include "buttons.h"
+#include "timer1_int.h"
+#else
+#include <stdio.h>
+
+struct wallclock_t {
+   unsigned char hour;
+   unsigned char sec;
+   unsigned char min;
+
+   unsigned int now;
+   unsigned int last;
+   unsigned int delta;
+   unsigned int accum;
+};
+
+struct sysData_t {
+   char name[32];
+   unsigned short badgeId; /* 2 bytes == our badge Id */
+   char sekrits[8];
+   char achievements[8];
+
+   /*
+      prefs
+   */
+   char ledBrightness;  /* 1 byte */
+   char backlight;      /* 1 byte */
+};
+
+struct sysData_t G_sysData;
+struct wallclock_t wclock;
+
+char G_button_cnt;
+char G_up_button_cnt;
+char G_down_button_cnt;
+char G_left_button_cnt;
+char G_right_button_cnt;
+int flashedBadgeId;
+int *G_flashAddr;
+void drawLCD8(unsigned char , int);
+char drbob[32];
+
+#endif
+
+// keep in sync with .h
+const char *ptrType[] = {
+    "var",
+    "func"
+};
+
+// keep in sync with .h
+const char *parmType[] = {
+    "void",
+    "char",
+    "char*",
+    "int",
+    "int*",
+};
+
+
+struct binding_t bindings[] = {
+    {.vf.cv  = (char *)&G_button_cnt},
+    {.vf.cv  = (char *)&G_up_button_cnt},
+    {.vf.cv  = (char *)&G_down_button_cnt},
+    {.vf.cv  = (char *)&G_left_button_cnt},
+    {.vf.cv  = (char *)&G_right_button_cnt},
+    {.vf.cv  = (char *)&(wclock.hour)},
+    {.vf.cv  = (char *)&(wclock.min)},
+    {.vf.cv  = (char *)&(wclock.sec)},
+    {.vf.iv  = (int *)&G_sysData.badgeId},
+    {.vf.cv  = (char *)G_sysData.name},
+    {.vf.iv  = (int *)&flashedBadgeId},
+    {.vf.iv  = (int *)&G_flashAddr},
+    {.vf.fun = (drawLCD8)},
+    {.vf.cv  = (char *)drbob},
+};
+
+struct param_t params[] = {
+    { "MAINBUTTON",	B_VARIABLE, B_CHAR,      B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "UPBUTTON",	B_VARIABLE, B_CHAR,      B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "DOWNBUTTON",	B_VARIABLE, B_CHAR,      B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "LEFTBUTTON",	B_VARIABLE, B_CHAR,      B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "RIGHTBUTTON",	B_VARIABLE, B_CHAR,      B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "HOUR", 		B_VARIABLE, B_CHAR,      B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "MIN", 		B_VARIABLE, B_CHAR,      B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "SEC", 		B_VARIABLE, B_CHAR,      B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "BADGEID", 	B_VARIABLE, B_INT,       B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "NAME", 		B_VARIABLE, B_CHARSTAR,  B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "FLASHEDID", 	B_VARIABLE, B_INT,       B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "FLASHADDR", 	B_VARIABLE, B_INT,       B_VOID,     B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "DRAWLCD8", 	B_FUNCTION, B_VOID,      B_CHAR,     B_INT,   B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+    { "DRBOB", 		B_VARIABLE, B_CHAR,      B_CHARSTAR, B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID,  B_VOID },
+};
+
+enum {
+    B_VAR,
+    B_FUNC
+};
+
+void listbindings()
+{
+    int i;
+
+    for (i=0; i< LASTBINDING; i++) {
+	if ( params[i].var_fun == B_FUNCTION ) {
+	    printf("%s %s(", parmType[params[i].ret], params[i].name);
+
+	    if (params[i].arg0 != B_VOID) {
+	        printf("%s", parmType[params[i].arg0]);
+
+	        if (params[i].arg1 != B_VOID) {
+	            printf(", %s", parmType[params[i].arg1]);
+
+	            if (params[i].arg2 != B_VOID) {
+	                printf(", %s", parmType[params[i].arg2]);
+
+			if (params[i].arg3 != B_VOID) {
+	                    printf(", %s", parmType[params[i].arg3]);
+
+			    if (params[i].arg4 != B_VOID) {
+	                        printf(", %s", parmType[params[i].arg4]);
+
+			        if (params[i].arg5 != B_VOID) {
+	                            printf(", %s", parmType[params[i].arg5]);
+
+			            if (params[i].arg6 != B_VOID) {
+	                                printf(", %s", parmType[params[i].arg6]);
+			            }
+			        }
+			    }
+			}
+		    }
+		}
+	    }
+
+	    printf(")\n");
+	}
+	else
+	if ( params[i].var_fun == B_VARIABLE ) {
+	    printf("%s %s\n", parmType[params[i].ret], params[i].name);
+	}
+    }
+}
+
+#ifdef MAIN
+void drawLCD8(unsigned char assetId, int frame)
+{
+
+}
+
+int main(int argc, char *argv[])
+{
+    char s[] = "hopadoop";
+
+
+    printf("sizeof fun_var %d\n", sizeof(union var_fun));
+    printf("sizeof binding %d\n", sizeof(struct binding_t));
+    listbindings();
+
+
+    bindings[DRAWLCD8].vf.fun(s, 0, 1, 2, 3, 4, 5, 6);
+
+    return(0);
+}
+#endif
