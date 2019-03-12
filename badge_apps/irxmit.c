HackRVA/badge2019interp

commit 26efe139504405f868b3e99809419921b8d890fc

Allow irxmit and lasertag to use serial port on linux
Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

badge_apps/irxmit.c
@@ -424,10 +424,13 @@ int irxmit_cb(void)
 int main(int argc, char *argv[])
 {
 
+	char *serial_port = NULL;
 #define IRXMIT_UDP_PORT 12345
 #define LASERTAG_UDP_PORT 12346
 
-	setup_linux_ir_simulator(LASERTAG_UDP_PORT, IRXMIT_UDP_PORT);
+	if (argc >= 2)
+		serial_port = argv[1];
+	setup_linux_ir_simulator(serial_port, LASERTAG_UDP_PORT, IRXMIT_UDP_PORT);
 	start_gtk(&argc, &argv, irxmit_cb, 30);
 	return 0;
 }

badge_apps/lasertag.c
@@ -727,13 +727,16 @@ int lasertag_cb(void)
 
 int main(int argc, char *argv[])
 {
+	char *serial_port = NULL;
 	argcount = argc;
 	arguments = argv;
 
 #define IRXMIT_UDP_PORT 12345
 #define LASERTAG_UDP_PORT 12346
 
-	setup_linux_ir_simulator(IRXMIT_UDP_PORT, LASERTAG_UDP_PORT);
+	if (argc >= 2)
+		serial_port = argv[1];
+	setup_linux_ir_simulator(serial_port, IRXMIT_UDP_PORT, LASERTAG_UDP_PORT);
 	start_gtk(&argc, &argv, lasertag_cb, 30);
 	return 0;
 }

commit 3211793333b70475f17d7311943db0914ffb3b6b

Redo changes from 26efe13 accidentally reverted by 5551209: irxmit.c, lasertag.c linux serial port code

badge_apps/irxmit.c
@@ -424,10 +424,13 @@ int irxmit_cb(void)
 int main(int argc, char *argv[])
 {
 
+	char *serial_port = NULL;
 #define IRXMIT_UDP_PORT 12345
 #define LASERTAG_UDP_PORT 12346
 
-	setup_linux_ir_simulator(LASERTAG_UDP_PORT, IRXMIT_UDP_PORT);
+	if (argc >= 2)
+		serial_port = argv[1];
+	setup_linux_ir_simulator(serial_port, LASERTAG_UDP_PORT, IRXMIT_UDP_PORT);
 	start_gtk(&argc, &argv, irxmit_cb, 30);
 	return 0;
 }

badge_apps/lasertag.c
@@ -727,13 +727,16 @@ int lasertag_cb(void)
 
 int main(int argc, char *argv[])
 {
+	char *serial_port = NULL;
 	argcount = argc;
 	arguments = argv;
 
 #define IRXMIT_UDP_PORT 12345
 #define LASERTAG_UDP_PORT 12346
 
-	setup_linux_ir_simulator(IRXMIT_UDP_PORT, LASERTAG_UDP_PORT);
+	if (argc >= 2)
+		serial_port = argv[1];
+	setup_linux_ir_simulator(serial_port, IRXMIT_UDP_PORT, LASERTAG_UDP_PORT);
 	start_gtk(&argc, &argv, lasertag_cb, 30);
 	return 0;
 }