d3d9e6e32d848ebc44b2d4ccb08a1d58c6eb2a6c
Stephen M. Cameron <stephenmcameron@gmail.com>
2019-04-28 16:17:58 -0400

    Prevent dead players from shooting
    Signed-off-by: Stephen M. Cameron <stephenmcameron@gmail.com>

--- badge_apps/lasertag.c
+++ badge_apps/lasertag.c
@@ -717,9 +717,11 @@ static void game_shoot(void)
 	unsigned int packet;
 	unsigned short payload;
 
-	payload = (OPCODE_HIT << 12) | ((G_sysData.badgeId & 0x1ff) << 4) | (team & 0x0f);
-	packet = build_ir_packet(0, 1, BADGE_IR_GAME_ADDRESS, BADGE_IR_BROADCAST_ID, payload);
-	send_ir_packet(packet);
+	if (suppress_further_hits_until < 0) { /* Player can only shoot if they are not currently dead. */
+		payload = (OPCODE_HIT << 12) | ((G_sysData.badgeId & 0x1ff) << 4) | (team & 0x0f);
+		packet = build_ir_packet(0, 1, BADGE_IR_GAME_ADDRESS, BADGE_IR_BROADCAST_ID, payload);
+		send_ir_packet(packet);
+	}
 
 	game_state = GAME_MAIN_MENU;
 }
